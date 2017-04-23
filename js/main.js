99dea07f92881b3165f0c070c078f3a0177590e1
asirivella <sirivella.ananda@gmail.com>
2017-04-23 01:22:49 -0400

    Fixing problems with tabs

--- js/main.js
+++ js/main.js
@@ -1,34 +1,18 @@
 $(document).ready( function() {
-
-	// Logo
-	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
-    }
-    
-	// Show logo 
-	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
-function animMeter(){
-    $(".meter > span").each(function() {
+	function animMeter(){
+		$(".meter > span").each(function() {
                 $(this)
                     .data("origWidth", $(this).width())
                     .width(0)
                     .animate({
                         width: $(this).data("origWidth")
                     }, 1200);
-            });
-}
-animMeter();
+            }
+		);
+	}
+	animMeter();
 
-      $('#tab-container').easytabs({
+    $('#tab-container').easytabs({
         animate			: true,
         updateHash		: true,
         transitionIn	: 'slideDown',
@@ -39,4 +23,5 @@ animMeter();
                     animMeter();
             }
         });
-    });
+    }
+);
